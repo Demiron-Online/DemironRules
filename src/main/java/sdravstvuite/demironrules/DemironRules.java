34d0370cb0702f65527078d068c75a6afef0805c
BuildTools <unconfigured@null.spigotmc.org>
2023-05-25 16:19:59 +0500

    Check

--- src/main/java/sdravstvuite/demironrules/DemironRules.java
+++ src/main/java/sdravstvuite/demironrules/DemironRules.java
@@ -1,13 +1,34 @@
 package sdravstvuite.demironrules;
 
-import org.bukkit.plugin.java.JavaPlugin;
 
-public final class DemironRules extends JavaPlugin {
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+import sdravstvuite.demironrules.EventListeners.*;
+
+import static sdravstvuite.demironrules.TownyPlugins.TownBlockTypes.registerCustomPlot;
+
+public final class DemironRules extends JavaPlugin implements Listener {
+    public static ItemStack AxeDrop;
+    private static DemironRules instance;
+    public void onLoad() {
+        instance = this;
+        registerCustomPlot();
+    }
+
+    public static DemironRules getInstance() {
+        return instance;
+    }
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        DemironRules plugin;
+        getServer().getPluginManager().registerEvents(new AxeEventListener(this), this);
+        getServer().getPluginManager().registerEvents(new WaterInDesertEventListener(this), this);
+        getServer().getPluginManager().registerEvents(new LavaEventListener(this), this);
+        getServer().getPluginManager().registerEvents(new FishingEventListener(this), this);
+        getServer().getPluginManager().registerEvents(new DeathEventListener(this), this);
     }
 
     @Override
